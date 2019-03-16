037e8f14ac5f2121bfa47a349cb33a3b101a3503
Stefano J. Attardi <stefano@attardi.org>
2019-03-16 15:57:52 -0700

    Fix bundling and dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,7 @@ export default [
   {
     input: './src/index.ts',
     output: [{file: './dist/index.js', format: 'cjs'}, {file: './dist/index.es.js', format: 'es'}],
+    external: ['rebound', 'react', 'react-dom'],
     plugins: [
       resolve({}),
       commonjs({
